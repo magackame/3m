a414b0fbb8746dbad0613a41e1db17818794c4da
Re10we <jekagorlov190@gmail.com>
2023-06-17 22:18:45 +0300

    Change interface, it now provides for several pages

--- client/src/window.rs
+++ client/src/window.rs
@@ -1,18 +1,54 @@
 use iced::executor;
 use iced::widget::{button, column, container};
 use iced::window::close;
-use iced::{Alignment, Application, window, Command, Element, Length, Theme};
+use iced::{window, Alignment, Application, Command, Element, Length, Theme};
 
-pub struct  Window {
-  
+enum Page {
+    StartMenu,
+    SinglePlayer,
+    Multiplayer,
+    Game,
+    Settings,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum Message {
+#[derive(Debug, Clone)]
+enum StartMenuMessage {
     SinglePlayerButtonPressed,
     MultiplayerButtonPressed,
     SettingsButtonPressed,
     ExitButtonPressed,
+}
+
+#[derive(Debug, Clone)]
+enum SinglePlayerMessage {
+    //TODO
+}
+
+#[derive(Debug, Clone)]
+enum MultiplayerMessage {
+    //TODO
+}
+
+#[derive(Debug, Clone)]
+enum GameMessage {
+    //TODO
+}
+
+#[derive(Debug, Clone)]
+enum SettingsMessage {
+    //TODO
+}
+
+pub struct Window {
+    current_page: Page,
+}
+#[derive(Debug, Clone)]
+pub enum Message {
+    StartMenu(StartMenuMessage),
+    SinglePlayer(SinglePlayerMessage),
+    Multiplayer(MultiplayerMessage),
+    Game(GameMessage),
+    Settings(SettingsMessage),
 }
 
 impl Application for Window {
@@ -22,8 +58,13 @@ impl Application for Window {
     type Message = Message;
 
     fn new(_flags: ()) -> (Self, Command<Self::Message>) {
-       //TODO create a constructor
-       (Self {}, window::change_mode(iced::window::Mode::Fullscreen))
+        //TODO create a constructor
+        (
+            Self {
+                current_page: Page::StartMenu,
+            },
+            window::change_mode(iced::window::Mode::Fullscreen),
+        )
     }
 
     fn title(&self) -> String {
@@ -32,42 +73,59 @@ impl Application for Window {
 
     fn update(&mut self, message: Message) -> Command<Message> {
         match message {
-            Message::SinglePlayerButtonPressed => {
+            // Block
+            Message::StartMenu(StartMenuMessage::SinglePlayerButtonPressed) => {
                 //TODO create event for Single Player button
+                Command::none()
             }
-            Message::MultiplayerButtonPressed => {
+            Message::StartMenu(StartMenuMessage::MultiplayerButtonPressed) => {
                 //TODO create event for Multiplayer button
+                Command::none()
             }
-            Message::SettingsButtonPressed => {
+            Message::StartMenu(StartMenuMessage::SettingsButtonPressed) => {
                 //TODO create event for Settings button
+                Command::none()
             }
-            Message::ExitButtonPressed => {
-                //TODO create event for Exit button
-                return close();
-            }
-        }
+            Message::StartMenu(StartMenuMessage::ExitButtonPressed) => window::close(),
 
-        Command::none()
+            //Next block
+            Message::SinglePlayer(_) => todo!(),
+            Message::Multiplayer(_) => todo!(),
+            Message::Game(_) => todo!(),
+            Message::Settings(_) => todo!(),
+        }
     }
 
     fn view(&self) -> Element<Message> {
-        let content = column![
-            button("Single player").on_press(Message::SinglePlayerButtonPressed),
-            button("Multiplayer").on_press(Message::MultiplayerButtonPressed),
-            button("Settings").on_press(Message::SettingsButtonPressed),
-            button("Exit").on_press(Message::ExitButtonPressed),
-        ]
-        .padding(20)
-        .spacing(20)
-        .align_items(Alignment::Center);
+        match self.current_page {
+            Page::StartMenu => {
+                let content = column![
+                    button("Single player").on_press(Message::StartMenu(
+                        StartMenuMessage::SinglePlayerButtonPressed
+                    )),
+                    button("Multiplayer").on_press(Message::StartMenu(
+                        StartMenuMessage::MultiplayerButtonPressed
+                    )),
+                    button("Settings")
+                        .on_press(Message::StartMenu(StartMenuMessage::SettingsButtonPressed)),
+                    button("Exit")
+                        .on_press(Message::StartMenu(StartMenuMessage::ExitButtonPressed)),
+                ]
+                .padding(20)
+                .spacing(20)
+                .align_items(Alignment::Center);
 
-        container(content)
-        .width(Length::Fill)
-        .height(Length::Fill)
-        .center_x()
-        .center_y()
-        .into()
+                container(content)
+                    .width(Length::Fill)
+                    .height(Length::Fill)
+                    .center_x()
+                    .center_y()
+                    .into()
+            }
+            Page::SinglePlayer => todo!(),
+            Page::Multiplayer => todo!(),
+            Page::Game => todo!(),
+            Page::Settings => todo!(),
+        }
     }
-
-    
 }
